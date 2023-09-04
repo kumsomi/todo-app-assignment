fa92c7d3f55a5b1c57019dc55b4e5ce5e3319b3d
Kum Somi <kumsomi.5@gmail.com>
2023-09-04 23:59:51 +0530

    bug fixes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,43 @@
-import { Link } from "react-router-dom";
-
-export const Navbar = () => {
+import { NavLink } from "react-router-dom";
+import React from "react";
+export const Navbar: React.FC = () => {
   return (
     <div className="navbar">
-      <Link className="link nav-brand" to="/">
+      <NavLink
+        // className={(isActive:boolean, isPending:boolean) =>
+        //   isPending
+        //     ? "link nav-brand"
+        //     : isActive
+        //     ? "link nav-brand selected"
+        //     : ""
+        // }
+        to="/"
+        // className="link nav-brand"
+        className={({ isActive, isPending }) =>
+          isPending
+            ? "link nav-brand"
+            : isActive
+            ? "link nav-brand link-active"
+            : "link nav-brand"
+        }
+      >
         tasktrip
-      </Link>
+      </NavLink>
       <nav className="nav-link">
-        <Link className="link" to="/todos">
+        <NavLink
+          className={({ isActive }) => (isActive ? "link-active" : "link")}
+          to="/todos"
+        >
           todos
-        </Link>
-        <Link className="link" to="/about">
+        </NavLink>
+        <NavLink
+          className={({ isActive, isPending }) =>
+            isActive ? "link-active" : "link"
+          }
+          to="/about"
+        >
           about
-        </Link>
+        </NavLink>
       </nav>
     </div>
   );
